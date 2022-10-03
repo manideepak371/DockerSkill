6065278407342140adf58f509f266dc8a56d0265
ManiDeepak <manideepak371@gmail.com>
2022-10-03 15:12:15 +0530

    move all to develop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import './App.css';
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [statuscode,setStatusCode]=useState()
+  const [flag,setFlag]=useState()
+  const [serverdata,setServerData]=useState([])
+  const [recordscount,setRecordsCount]=useState(5)
+  // useEffect(()=>{
+  //   const text= statuscode !== 200 ? "failed" : "success"
+  //   setFlag(text)
+    
+  // },[statuscode])
+
+  const FetchData=async (url)=>{
+    // axios.get('http://localhost:9000').then((res)=>(setStatusCode(res.status.code))).catch(err => console.log(err))
+    const data=await axios.get(url)
+    const responsedata=data.data
+    setServerData(responsedata.results)
+    console.log(responsedata.results)
+  }
+
+  const RecordsHandler=(e)=>{
+    console.log(e.target.value)
+    setRecordsCount(e.target.value)
+  }
+
+  useEffect(()=>{
+    const url=`https://randomuser.me/api?results=${recordscount}`
+    FetchData(url)
+  },[recordscount])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-            hello through docker changed code
-        </p>
-      </header>
-      <body>
-        <p>check git commands flow pull, only to create  pull request</p>
-      </body> 
+      {/* <p>{flag}</p> */}
+      <button onClick={FetchData}>Click</button>
+        <select onChange={(event)=>{RecordsHandler(event)}}>
+          <option>5</option>
+          <option>10</option>
+          <option>15</option>
+        </select>
+      {
+        serverdata.length > 0 &&
+          <table>
+            {serverdata.map((person,index)=>(
+              <tr>
+                <td>{person.gender}</td>
+                <td>{person.name.last}, {person.name.first}</td>
+                <td>{person.city}</td>
+              </tr>
+            ))}
+          </table>
+      }
     </div>
   );
 }
 
 export default App;
+
+
+
+// input ({ref}) --> ref.current.focus()
+// input {onchange} -->
+
+// function()()()
+
+
+//i18n (languages)
